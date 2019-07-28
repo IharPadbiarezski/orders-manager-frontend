df64f9721386d2507da784c15cccea3ddc52aefc
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-07-28 22:18:45 +0300

    Updates

---
+++ precache-manifest.881fe007e27439dacc27895c307dd7b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e1ce915d42632b6f9d0feb8f3143a1",
+    "url": "/orders-manager-frontend/index.html"
+  },
+  {
+    "revision": "88932621439eb2c3a1e7",
+    "url": "/orders-manager-frontend/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/orders-manager-frontend/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "88932621439eb2c3a1e7",
+    "url": "/orders-manager-frontend/static/js/main.2ada46c7.chunk.js"
+  },
+  {
+    "revision": "07e47a11660c5e2e47b5",
+    "url": "/orders-manager-frontend/static/js/runtime~main.9f74f02a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/orders-manager-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
