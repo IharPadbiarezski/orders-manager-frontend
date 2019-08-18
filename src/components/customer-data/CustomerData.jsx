a4a0e62a36e74cf04aa070e3cf1d7110a78b75b1
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-08-18 15:29:53 +0300

    refactor: remove redundant div.address wrapper

--- src/components/customer-data/CustomerData.jsx
+++ src/components/customer-data/CustomerData.jsx
@@ -10,42 +10,40 @@ function CustomerData() {
       <LabelledInput label="Время доставки" />
       <LabelledInput label="Телефон" />
       <LabelledInput label="Доп. телефон" />
-      <div className="address">
-        <LabelledInput label="Индекс" type="number" />
-        <LabelledInput label="Область" />
-        <LabelledInput label="Город" />
-        <LabelledInput
-          label="Тип улицы"
-          renderInput={() => (
-            <select>
-              <option value="ул">улица</option>
-              <option value="пер">переулок</option>
-              <option value="пр">проезд</option>
-              <option value="бул">бульвар</option>
-              <option value="пл">площадь</option>
-              <option value="тр">тракт</option>
-              <option value="шс">шоссе</option>
-            </select>
-          )}
-        />
-        <LabelledInput label="Улица" />
-        <LabelledInput label="Дом" />
-        <LabelledInput label="Корпус" />
-        <LabelledInput label="Квартира" type="number" />
-        <LabelledInput label="Этаж" type="number" />
-        <LabelledInput label="Подъезд" type="number" />
-        <LabelledInput
-          label="Комментарий"
-          renderInput={() => (
-            <textarea
-              id="order-comment-area"
-              cols="30"
-              rows="3"
-              className="comment-area"
-            />
-          )}
-        />
-      </div>
+      <LabelledInput label="Индекс" type="number" />
+      <LabelledInput label="Область" />
+      <LabelledInput label="Город" />
+      <LabelledInput
+        label="Тип улицы"
+        renderInput={() => (
+          <select>
+            <option value="ул">улица</option>
+            <option value="пер">переулок</option>
+            <option value="пр">проезд</option>
+            <option value="бул">бульвар</option>
+            <option value="пл">площадь</option>
+            <option value="тр">тракт</option>
+            <option value="шс">шоссе</option>
+          </select>
+        )}
+      />
+      <LabelledInput label="Улица" />
+      <LabelledInput label="Дом" />
+      <LabelledInput label="Корпус" />
+      <LabelledInput label="Квартира" type="number" />
+      <LabelledInput label="Этаж" type="number" />
+      <LabelledInput label="Подъезд" type="number" />
+      <LabelledInput
+        label="Комментарий"
+        renderInput={() => (
+          <textarea
+            id="order-comment-area"
+            cols="30"
+            rows="3"
+            className="comment-area"
+          />
+        )}
+      />
     </div>
   )
 }
