1864f68a62fbd6004177e91276a2eb6dc4513a8d
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-08-28 22:02:22 +0300

    refactor: integrate Button component into Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Nav.css'
 import '../../assets/fontello/css/fontello.css'
+import Button from '../button'
 
 function Nav() {
   return (
     <nav className="nav" id="nav">
-      <button type="button" id="menu-toggle">
+      <Button className="btn-menu-toggle">
         <i className="icon-menu" />
-      </button>
+      </Button>
       <ul className="menu">
         <li>
           <a href="/add_order">Добавить заказ</a>
