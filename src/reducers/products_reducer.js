b24c5476251b0e9b1982ff6ca665cfa186a7ae43
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-09-01 22:12:40 +0300

    test: add tests for products reducer
    Extract it to separate file

---
+++ src/reducers/products_reducer.js
@@ -0,0 +1,23 @@
+import { ADD_PRODUCT, REMOVE_PRODUCT } from '../actions'
+
+const initialState = [{ id: 1 }]
+
+function getMaximumProductId(products) {
+  const ids = products.map((product) => product.id)
+  return Math.max(...ids)
+}
+
+export default function productsReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_PRODUCT: {
+      const newProductId = getMaximumProductId(state) + 1
+      return [...state, { id: newProductId }]
+    }
+    case REMOVE_PRODUCT:
+      return state.filter(({ id }) => id !== action.payload.id)
+    default:
+      return state
+  }
+}
+
+export { getMaximumProductId }
--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -1,26 +1,6 @@
 import { combineReducers } from 'redux'
-import { ADD_PRODUCT, REMOVE_PRODUCT } from '../actions'
+import productsReducer from './products_reducer'
 
-const initialState = [{ id: 1 }]
+const rootReducer = combineReducers({ products: productsReducer })
 
-function getMaximumProductId(products) {
-  const ids = products.map((product) => product.id)
-  return Math.max(...ids)
-}
-
-export function productsReducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD_PRODUCT: {
-      const newProductId = getMaximumProductId(state) + 1
-      return [...state, { id: newProductId }]
-    }
-    case REMOVE_PRODUCT:
-      return state.filter(({ id }) => id !== action.payload.id)
-    default:
-      return state
-  }
-}
-
-const app = combineReducers({ products: productsReducer })
-
-export default app
+export default rootReducer
