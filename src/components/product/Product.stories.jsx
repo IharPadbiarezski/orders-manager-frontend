c6b0aa5a2d082948082a19c7d371cca5de2780a7
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-09-14 17:20:41 +0300

    chore: remove redundant Product children

--- src/components/product/Product.stories.jsx
+++ src/components/product/Product.stories.jsx
@@ -4,15 +4,11 @@ import { action } from '@storybook/addon-actions'
 import Product from './Product'
 
 storiesOf('Product', module)
-  .add('simple', () => (
-    <Product onRemove={action('removed')}>Some nice product</Product>
-  ))
+  .add('simple', () => <Product onRemove={action('removed')} />)
   .add('with product types', () => (
     <Product
       onRemove={action('removed')}
       types={[{ id: 1, value: 'first' }, { id: 2, value: 'second' }]}
       onSelectProductType={action('selected product type')}
-    >
-      Another nice product
-    </Product>
+    />
   ))
