869a99b7a966b180fecedf9d53fb751ad5407387
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-09-20 23:29:03 +0300

    refactor: move mock types & parameters to product

--- src/models/product.js
+++ src/models/product.js
@@ -7,3 +7,71 @@ const createProduct = ({ id, optionsMode = OPTIONS_MODES.TYPES, type }) => ({
 })
 
 export default createProduct
+
+const mockProductTypes = [
+  { id: 1, value: 'одеяло' },
+  { id: 2, value: 'подушка' },
+  { id: 3, value: 'постельное бельё' },
+  { id: 4, value: 'простыня на резинке' },
+  { id: 5, value: 'наматрасник' },
+  { id: 6, value: 'плед' },
+  { id: 7, value: 'полотенце' },
+]
+
+const mockProductParameters = [
+  {
+    name: 'размер',
+    options: [
+      {
+        text: 'M',
+        value: 'M',
+      },
+      {
+        text: 'L',
+        value: 'L',
+      },
+    ],
+  },
+  {
+    name: 'материал',
+    options: [
+      {
+        text: 'хлопок',
+        value: 'хлопок',
+      },
+      {
+        text: 'шелк',
+        value: 'шелк',
+      },
+    ],
+  },
+  {
+    name: 'производитель',
+    options: [
+      {
+        text: 'беларусь',
+        value: 'беларусь',
+      },
+    ],
+  },
+  {
+    name: 'плотность',
+    options: [
+      {
+        text: 'плотный',
+        value: 'плотный',
+      },
+    ],
+  },
+  {
+    name: 'тип',
+    options: [
+      {
+        text: 'важный',
+        value: 'важный',
+      },
+    ],
+  },
+]
+
+export { mockProductTypes, mockProductParameters }
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,75 +1,14 @@
 import { ADD_PRODUCT, REMOVE_PRODUCT, SELECT_PRODUCT_TYPE } from '../actions'
 import { OPTIONS_MODES } from '../components/product/Product'
-import createProduct from '../models/product'
-
-const productTypes = [
-  { id: 1, value: 'одеяло' },
-  { id: 2, value: 'подушка' },
-  { id: 3, value: 'постельное бельё' },
-  { id: 4, value: 'простыня на резинке' },
-  { id: 5, value: 'наматрасник' },
-  { id: 6, value: 'плед' },
-  { id: 7, value: 'полотенце' },
-]
+import createProduct, {
+  mockProductParameters,
+  mockProductTypes,
+} from '../models/product'
 
 const initialState = {
   items: [createProduct({ id: 1 })],
-  types: productTypes,
-  parameters: [
-    {
-      name: 'размер',
-      options: [
-        {
-          text: 'M',
-          value: 'M',
-        },
-        {
-          text: 'L',
-          value: 'L',
-        },
-      ],
-    },
-    {
-      name: 'материал',
-      options: [
-        {
-          text: 'хлопок',
-          value: 'хлопок',
-        },
-        {
-          text: 'шелк',
-          value: 'шелк',
-        },
-      ],
-    },
-    {
-      name: 'производитель',
-      options: [
-        {
-          text: 'беларусь',
-          value: 'беларусь',
-        },
-      ],
-    },
-    {
-      name: 'плотность',
-      options: [
-        {
-          text: 'плотный',
-          value: 'плотный',
-        },
-      ],
-    },
-    {
-      name: 'тип',
-      options: [
-        {
-          text: 'важный',
-          value: 'важный',
-        },
-      ],
-    },
-  ],
+  types: mockProductTypes,
+  parameters: mockProductParameters,
 }
 
 function getMaximumProductId(products) {
