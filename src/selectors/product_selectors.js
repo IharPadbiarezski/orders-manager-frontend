9fe323f9cfc02fb8511162c41d8674f61bb729d6
Dima Krutolianov <DimaKrutDK@ya.ru>
2019-09-22 15:32:45 +0300

    refactor: reuse selectProducts selector in another

--- src/selectors/product_selectors.js
+++ src/selectors/product_selectors.js
@@ -1,8 +1,8 @@
 export const selectMockTypes = (state) => state.products.types
 
+export const selectProducts = (state) => state.products.items
+
 export const selectOptionsMode = (state, productId) =>
-  state.products.items.find((product) => product.id === productId).optionsMode
+  selectProducts(state).find((product) => product.id === productId).optionsMode
 
 export const selectMockParameters = (state) => state.products.parameters
-
-export const selectProducts = (state) => state.products.items
